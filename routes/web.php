5ce4044704a2268dd39452de61e75c7566adc35b
rkavidu222 <123630912+rkavidu222@users.noreply.github.com>
2025-08-25 15:44:17 +0530

    create homecontroller

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
+Route::get('/home', [HomeController::class, 'index'])->name('home');
+
+
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
@@ -26,3 +31,5 @@ Route::middleware([
         return view('dashboard');
     })->name('dashboard');
 });
+
+
